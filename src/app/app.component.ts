f02488b336a31bf6fda1a9b5db9b4d5074160c86
udd_devel <diegoortega@udd.cl>
2024-09-17 22:49:00 -0300

    Feat: Cambios en Header y background color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CountdownComponent } from "./countdown/countdown.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CountdownComponent],
+  imports: [RouterOutlet, CountdownComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
